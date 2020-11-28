cc668ac408b5532d072bce84e7f4b961f384c21e
z3r0spam <danbooru@localhost.localdomain>
2020-11-28 09:58:28 +0100

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-set :user, "danbooru"
+set :user, "zerox"
 set :rails_env, "production"
 set :delayed_job_workers, 8
 append :linked_files, ".env.production"
 
-server "localhost", :roles => %w(web app cron), :primary => true, :user => "danbooru"
+server "localhost", :roles => %w(web app cron), :primary => true, :user => "zerox"
 
 set :newrelic_appname, "Danbooru"
 after "deploy:finished", "newrelic:notice_deployment"
